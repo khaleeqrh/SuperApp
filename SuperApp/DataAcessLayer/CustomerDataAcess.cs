bb57f0d9f64aa0d3bfca3c9a64e978708cf0c824
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2020-08-18 21:08:58 +0500

    Prevented SQL Injection

--- SuperApp/DataAcessLayer/CustomerDataAcess.cs
+++ SuperApp/DataAcessLayer/CustomerDataAcess.cs
@@ -25,7 +25,7 @@ namespace SuperApp.DataAcessLayer
         {
             using (IDbConnection cnn = new SQLiteConnection(@"Data Source =.\chinook.db; Version = 3;"))
             {
-                var data = cnn.Query<CustomerModel>($"SELECT * FROM Customers WHERE CustomerId = {id}").SingleOrDefault();
+                var data = cnn.Query<CustomerModel>($"SELECT * FROM Customers WHERE CustomerId = @ID;", new { ID = id }).SingleOrDefault();
                 return data;
             }
             
