9bde9a8533e9b436781eaf36f199407314e015de
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2020-08-18 17:10:58 +0500

    Added Rola

--- SuperApp/Controllers/WeatherForecastController.cs
+++ SuperApp/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace SuperApp.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            var Person = new Person();
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
